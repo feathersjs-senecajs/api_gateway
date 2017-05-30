b40828fd621e6b181f55e316a8d80a559f7ed16c
Celso Bring <cbringf@gmail.com>
2017-05-30 17:32:09 -0500

    creates points service adds service, hooks and filters for points endpoint adds nedb package

---
+++ src/services/points/points.service.js
@@ -0,0 +1,29 @@
+// Initializes the `points` service on path `/points`
+const createService = require('feathers-nedb');
+const createModel = require('../../models/points.model');
+const hooks = require('./points.hooks');
+const filters = require('./points.filters');
+
+module.exports = function () {
+  const app = this;
+  const Model = createModel(app);
+  const paginate = app.get('paginate');
+
+  const options = {
+    name: 'points',
+    Model,
+    paginate
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/points', createService(options));
+
+  // Get our initialized service so that we can register hooks and filters
+  const service = app.service('points');
+
+  service.hooks(hooks);
+
+  if (service.filter) {
+    service.filter(filters);
+  }
+};
