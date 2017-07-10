109dd8331b3b10a8fd8f16e94dd4907f7921f291
Celso Bring <cbringf@gmail.com>
2017-07-10 16:51:34 -0500

    adds poi-category service

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const points = require('./points/points.service.js');
 const file = require('./file/file.service.js');
+const poiCategory = require('./poi-category/poi-category.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(points);
   app.configure(file);
+  app.configure(poiCategory);
 };
