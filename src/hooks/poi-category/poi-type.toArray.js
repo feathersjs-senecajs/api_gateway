0db63fb025c91af742acea837e95335c84c3569a
Celso Bring <cbringf@gmail.com>
2017-07-21 16:16:35 -0500

    fixes bug to get result data of hook.result

--- src/hooks/poi-category/poi-type.toArray.js
+++ src/hooks/poi-category/poi-type.toArray.js
@@ -1,9 +1,9 @@
 module.exports = function () {
 	return function (hook) {
-		var poiTypes = hook.result.poi_types;
+		var poiTypes = hook.result.data[0].poi_types;
 
-		if (poiTypes && !poiTypes.constructor === Array) {
-			hook.result.poi_types = [poiTypes];
+		if (poiTypes && poiTypes.constructor === Object) {
+			hook.result.data[0].poi_types = [poiTypes];
 		}
 	}
 }
