ef0d0b88755c258a06fa7ce78f479584411323e8
Celso Bring <cbringf@gmail.com>
2017-07-25 18:42:56 -0500

    adds geoJSON to points service response

--- src/hooks/geojson.js
+++ src/hooks/geojson.js
@@ -1,9 +1,26 @@
 module.exports = function () {
 	function buildGeoJSON(data) {
-
+		return {
+			overlays: [{
+				id: "Wifi free",
+				category: "green",
+				points: data.map(item => new Object({ 
+					type: "Feature",
+					properties: {
+						name: item.name
+					},
+					geometry: {
+						type: "Point",
+						coordinates: [item.lng, item.lat]
+					}
+				}))
+			}]};
 	}
 
 	return function (hook) { 
-		hook.result.data.push(buildGeoJSON(hook.result.data));
+		hook.result.data = {
+			poiList: hook.result.data,
+			geoJSON: buildGeoJSON(hook.result.data)
+		};
 	};
 };
