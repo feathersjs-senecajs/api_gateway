2db14f2d5c6ada6567857989a7a5e728e0bef719
Celso Bring <cbringf@gmail.com>
2017-08-10 10:21:56 -0500

    fixes events grouping to output pagination variables

--- src/hooks/group.events.js
+++ src/hooks/group.events.js
@@ -3,8 +3,9 @@ var _ = require('lodash');
 module.exports = function () { 
 	return function (hook) {
 		let data = hook.result.data || [hook.result];
-		hook.result = data.map((item) => {
-			
+		let aux = hook.result.data || hook.result;
+
+		aux = data.map((item) => {
 			item.events = item.events.map(w => {
 				return w.eventIds.map(id =>
 					item.poi_events[_.findIndex(item.poi_events, { _id: id })]
