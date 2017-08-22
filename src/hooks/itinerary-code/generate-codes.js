20c2e9475507904f380deb01213aac371e425ac3
Celso Bring <cbringf@gmail.com>
2017-08-22 16:15:27 -0500

    avoids generate-codes hook, for testing purposes

--- src/hooks/itinerary-code/generate-codes.js
+++ src/hooks/itinerary-code/generate-codes.js
@@ -1,28 +1,28 @@
 const defaultCode = require('../../defaults').itineraryCode;
 const codeLimit = require('../../defaults').codeLimit;
-const seneca = require('seneca')()
-	.use('seneca-amqp-transport')
-	.client({
-		type: 'amqp',
-		pin: 'cmd:get,count:*',
-		url: 'amqp://localhost:5672'
-	});
+// const seneca = require('seneca')()
+// 	.use('seneca-amqp-transport')
+// 	.client({
+// 		type: 'amqp',
+// 		pin: 'cmd:get,count:*',
+// 		url: 'amqp://localhost:5672'
+// 	});
 
 module.exports = function (options = {}) {
 	return async function (hook) {
-		let codesSvc = hook.service;
-		let items = await codesSvc.find({ $limit: 0 });
+		// let codesSvc = hook.service;
+		// let items = await codesSvc.find({ $limit: 0 });
 
-		if (items.total < codeLimit) {
-			seneca.act({ cmd: 'get', count: codeLimit }, async (err, msg) => {
-				msg.codes.filter(c => c !== defaultCode)
-					.forEach((item, index) => {
-						codesSvc.create({
-							code: item
-						});
-					});
-				seneca.close();//todo: check if need to close seneca 
-			})
-		};
+		// if (items.total < codeLimit) {
+		// 	seneca.act({ cmd: 'get', count: codeLimit }, async (err, msg) => {
+		// 		msg.codes.filter(c => c !== defaultCode)
+		// 			.forEach((item, index) => {
+		// 				codesSvc.create({
+		// 					code: item
+		// 				});
+		// 			});
+		// 		seneca.close();//todo: check if need to close seneca 
+		// 	})
+		// };
 	};
 };
