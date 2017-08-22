41302365b0243012945f7d2ce2fed3764618eff0
Celso Bring <cbringf@gmail.com>
2017-08-22 15:46:15 -0500

    adds refactoring

--- src/services/itinerary-code/itinerary-code.hooks.js
+++ src/services/itinerary-code/itinerary-code.hooks.js
@@ -1,7 +1,5 @@
 const generateCodes = require('../../hooks/itinerary-code/generate-codes');
 const popCodes = require('../../hooks/itinerary-code/pop-code');
-const iff = require('feathers-hooks-common').iff;
-const isProvider = require('feathers-hooks-common').isProvider;
 
 module.exports = {
 	before: {
