2697ce4e1ce2247cae3cd1d808c99a8f229fd017
Celso Bring <cbringf@gmail.com>
2017-08-29 14:52:04 -0500

    fixes bug when assign pax codes

--- src/hooks/reservation-itinerary-bind/assign-codes.js
+++ src/hooks/reservation-itinerary-bind/assign-codes.js
@@ -3,7 +3,7 @@ module.exports = function () {
 		let paxSvc = hook.app.service('pax');
 		let paxList = await paxSvc.find();
 
-		paxList.forEach((item, index) => { 
+		paxList.data.forEach((item, index) => { 
 			item.code = hook.data.itineraryCodes[index];
 			paxSvc.update(item._id, item);
 		});
