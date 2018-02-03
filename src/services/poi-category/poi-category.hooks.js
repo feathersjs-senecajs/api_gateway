32eafc41c4449229ed55969c5e7c40bbce9fd722
Celso Bring <cbringf@gmail.com>
2018-02-03 14:31:37 -0500

    removes bundle hook

--- src/services/poi-category/poi-category.hooks.js
+++ src/services/poi-category/poi-category.hooks.js
@@ -25,7 +25,7 @@ module.exports = {
 
 	after: {
 		all: [],
-		find: [populate({ schema: populateSchema }), bundle()],
+		find: [populate({ schema: populateSchema })],
 		get: [populate({ schema: populateSchema })],
 		create: [],
 		update: [],
