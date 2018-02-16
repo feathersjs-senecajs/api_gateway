5eeab0f43181b80ad671d088d81e7a6dad06da96
Celso Bring <cbringf@gmail.com>
2018-02-16 13:21:11 -0500

    adds uri field to poi-category and poi-type

--- src/services/poi-type/poi-type.hooks.js
+++ src/services/poi-type/poi-type.hooks.js
@@ -1,6 +1,7 @@
-const ajv = require("ajv");
-const validateSchema = require("feathers-hooks-common").validateSchema;
-const schema = require("../../models/schemas/poi-type/poi-type");
+const ajv = require('ajv');
+const validateSchema = require('feathers-hooks-common').validateSchema;
+const schema = require('../../models/schemas/poi-type/poi-type');
+const createUri = require('../../hooks/shared/uri.creator');
 
 const { authenticate } = require("feathers-authentication").hooks;
 const restrictToRoles = require("../role-filter");
@@ -18,11 +19,13 @@ module.exports = {
 		get: [restrictToRoles([roles.ADMIN, roles.OP, roles.GIPSI])],
 		create: [
 			restrictToRoles([roles.ADMIN, roles.OP]),
-			validateSchema(schema, ajv)
+			validateSchema(schema, ajv),
+			createUri()
 		],
 		update: [
 			restrictToRoles([roles.ADMIN, roles.OP]),
-			validateSchema(schema, ajv)
+			validateSchema(schema, ajv),
+			createUri()
 		],
 		patch: [
 			restrictToRoles([roles.ADMIN, roles.OP]),
