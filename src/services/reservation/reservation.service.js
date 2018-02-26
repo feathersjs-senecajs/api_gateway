41b5b77c8613b728aa1d619979f8a645779084de
Celso Bring <cbringf@gmail.com>
2018-02-26 15:54:11 -0500

    fixes a bug emiting ReservationBound event adds refactoring

--- src/services/reservation/reservation.service.js
+++ src/services/reservation/reservation.service.js
@@ -1,29 +1,30 @@
 // Initializes the `reservation` service on path `/reservation`
-const createService = require('feathers-nedb');
-const createModel = require('../../models/reservation.model');
-const hooks = require('./reservation.hooks');
-const filters = require('./reservation.filters');
+const createService = require("feathers-nedb");
+const createModel = require("../../models/reservation.model");
+const hooks = require("./reservation.hooks");
+const filters = require("./reservation.filters");
 
-module.exports = function () {
-  const app = this;
-  const Model = createModel(app);
-  const paginate = app.get('paginate');
+module.exports = function() {
+	const app = this;
+	const Model = createModel(app);
+	const paginate = app.get("paginate");
 
-  const options = {
-    name: 'reservation',
-    Model,
-    paginate
-  };
+	const options = {
+		name: "reservation",
+		Model,
+		paginate,
+		events: ['bound']
+	};
 
-  // Initialize our service with any options it requires
-  app.use('/reservation', createService(options));
+	// Initialize our service with any options it requires
+	app.use("/reservation", createService(options));
 
-  // Get our initialized service so that we can register hooks and filters
-  const service = app.service('reservation');
+	// Get our initialized service so that we can register hooks and filters
+	const service = app.service("reservation");
 
-  service.hooks(hooks);
+	service.hooks(hooks);
 
-  if (service.filter) {
-    service.filter(filters);
-  }
+	if (service.filter) {
+		service.filter(filters);
+	}
 };
