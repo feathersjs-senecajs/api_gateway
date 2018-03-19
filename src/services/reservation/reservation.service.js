90a2c52ba5cec1bdda89bb1beed50cff31f4944e
alejandrohn <alehn96@gmail.com>
2018-03-19 12:38:43 -0400

    add mongo to reservation service

--- src/services/reservation/reservation.service.js
+++ src/services/reservation/reservation.service.js
@@ -1,25 +1,28 @@
 // Initializes the `reservation` service on path `/reservation`
-const createService = require('feathers-nedb');
+// const createService = require('feathers-nedb');
 const createModel = require('../../models/reservation.model');
 const hooks = require('./reservation.hooks');
 const filters = require('./reservation.filters');
+const createService = require('feathers-mongodb');
 
 module.exports = function () {
   const app = this;
-  const Model = createModel(app);
+  //const Model = createModel(app);
   const paginate = app.get('paginate');
-
+  const mongoClient = app.get('mongoClient')
   const options = {
-    name: 'reservation',
-    Model,
-    paginate
+	  paginate
   };
 
   // Initialize our service with any options it requires
   app.use('/reservation', createService(options));
 
   // Get our initialized service so that we can register hooks and filters
-  const service = app.service('reservation');
+	const service = app.service('reservation');
+
+	mongoClient.then(client => {
+		service.Model = client.db('gipsi').collection('reservation');
+	})
 
   service.hooks(hooks);
 
