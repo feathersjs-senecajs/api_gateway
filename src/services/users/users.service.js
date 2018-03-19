e2a3dc334c9a3151a027b2912ff153bd03ae96e0
alejandrohn <alehn96@gmail.com>
2018-03-19 17:22:13 -0400

    downgrade service users to nedb

--- src/services/users/users.service.js
+++ src/services/users/users.service.js
@@ -1,31 +1,29 @@
 // Initializes the `users` service on path `/users`
-// const createService = require('feathers-nedb');
+const createService = require('feathers-nedb');
 const createModel = require('../../models/users.model');
 const hooks = require('./users.hooks');
 const filters = require('./users.filters');
-const createService = require('feathers-mongodb');
 
 module.exports = function () {
-	const app = this;
-	//const Model = createModel(app);
-	const paginate = app.get('paginate');
-	const mongoClient = app.get('mongoClient')
-	const options = {
-		paginate
-	};
+  const app = this;
+  const Model = createModel(app);
+  const paginate = app.get('paginate');
 
-	// Initialize our service with any options it requires
-	app.use('/users', createService(options));
+  const options = {
+    name: 'users',
+    Model,
+    paginate
+  };
 
-	// Get our initialized service so that we can register hooks and filters
-	const service = app.service('users');
-	
-	mongoClient.then(client => {
-		service.Model = client.db('gipsi').collection('users');
-	});
-	service.hooks(hooks);
+  // Initialize our service with any options it requires
+  app.use('/users', createService(options));
 
-	if (service.filter) {
-		service.filter(filters);
-	}
+  // Get our initialized service so that we can register hooks and filters
+  const service = app.service('users');
+
+  service.hooks(hooks);
+
+  if (service.filter) {
+    service.filter(filters);
+  }
 };
