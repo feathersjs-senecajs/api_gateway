287596abc95143d678c14bff543620db0f29b4c0
Celso Bring <cbringf@gmail.com>
2018-03-19 16:58:52 -0500

    fixes a bug in populate events hook

--- src/hooks/itinerary/populate.events.js
+++ src/hooks/itinerary/populate.events.js
@@ -1,7 +1,7 @@
 module.exports = function () { 
 	return function populateEvents(hook) { 
 		const poiEventSvc = hook.app.service('poi-event');
-		const events = hook.itinerary.events.map(async e => { 
+		const events = hook.result.events.map(async e => { 
 			e.eventIds = await poiEventSvc.find({
 				query: {
 					_id: {
