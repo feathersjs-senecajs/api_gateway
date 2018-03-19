b2df9a6a633c7f044c7d7172ca33eb3852fa7b97
alejandrohn <alehn96@gmail.com>
2018-03-19 12:46:30 -0400

    add mongodb to pax service

--- src/services/pax/pax.service.js
+++ src/services/pax/pax.service.js
@@ -1,29 +1,30 @@
 // Initializes the `pax` service on path `/pax`
-const createService = require('feathers-nedb');
+//const createService = require('feathers-nedb');
 const createModel = require('../../models/pax.model');
 const hooks = require('./pax.hooks');
 const filters = require('./pax.filters');
+const createService = require('feathers-mongodb');
 
 module.exports = function () {
-  const app = this;
-  const Model = createModel(app);
-  const paginate = app.get('paginate');
+	const app = this;
+	//  const Model = createModel(app);
+	const paginate = app.get('paginate');
+	const mongoClient = app.get('mongoClient');
+	const options = { paginate };
 
-  const options = {
-    name: 'pax',
-    Model,
-    paginate
-  };
 
-  // Initialize our service with any options it requires
-  app.use('/pax', createService(options));
+	// Initialize our service with any options it requires
+	app.use('/pax', createService(options));
 
-  // Get our initialized service so that we can register hooks and filters
-  const service = app.service('pax');
+	// Get our initialized service so that we can register hooks and filters
+	const service = app.service('pax');
 
-  service.hooks(hooks);
+	mongoClient.then(client => {
+		service.Model = client.db('gipsi').collection('pax');
+	})
+	service.hooks(hooks);
 
-  if (service.filter) {
-    service.filter(filters);
-  }
+	if (service.filter) {
+		service.filter(filters);
+	}
 };
