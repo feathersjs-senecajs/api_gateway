b7c78efb00e0f2f97e7dc63612fa0d9bc71f39fd
alejandrohn <alehn96@gmail.com>
2018-03-19 12:44:39 -0400

    add mongodb to itinerary-code

--- src/services/itinerary-code/itinerary-code.service.js
+++ src/services/itinerary-code/itinerary-code.service.js
@@ -1,29 +1,30 @@
 // Initializes the `itineraryCode` service on path `/itinerary-code`
-const createService = require('feathers-nedb');
+// const createService = require('feathers-nedb');
 const createModel = require('../../models/itinerary-code.model');
 const hooks = require('./itinerary-code.hooks');
 const filters = require('./itinerary-code.filters');
+const createService = require('feathers-mongodb');
 
 module.exports = function () {
-  const app = this;
-  const Model = createModel(app);
-  const paginate = app.get('paginate');
+	const app = this;
+	//const Model = createModel(app);
+	const paginate = app.get('paginate');
+	const mongoClient = app.get('mongoClient');
+	const options = { paginate };
 
-  const options = {
-    name: 'itinerary-code',
-    Model,
-    paginate
-  };
+	// Initialize our service with any options it requires
+	app.use('/itinerary-code', createService(options));
 
-  // Initialize our service with any options it requires
-  app.use('/itinerary-code', createService(options));
+	// Get our initialized service so that we can register hooks and filters
+	const service = app.service('itinerary-code');
 
-  // Get our initialized service so that we can register hooks and filters
-  const service = app.service('itinerary-code');
+	mongoClient.then(client => {
+		service.Model = client.db('gipsi').collection('itinerary_code');
+	})
 
-  service.hooks(hooks);
+	service.hooks(hooks);
 
-  if (service.filter) {
-    service.filter(filters);
-  }
+	if (service.filter) {
+		service.filter(filters);
+	}
 };
